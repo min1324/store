74782d1649b2198a2543309afbf19b96e0fa30b8
min <min1324@icloud.com>
2022-03-28 08:37:50 +0800

    add ptr2any

--- store.go
+++ store.go
@@ -13,13 +13,21 @@ type Entry struct {
 
 type any = interface{}
 
-// Load returns the value set by the most recent Store.
-func (e *Entry) Load() (val any) {
-	p := atomic.LoadPointer(&e.p)
+func ptr2any(p unsafe.Pointer) any {
 	if p == nil {
 		return nil
 	}
 	return *(*any)(p)
+}
+
+// Load returns the value set by the most recent Store.
+func (e *Entry) Load() (val any) {
+	// p := atomic.LoadPointer(&e.p)
+	// if p == nil {
+	// 	return nil
+	// }
+	// return *(*any)(p)
+	return ptr2any(atomic.LoadPointer(&e.p))
 }
 
 // Store sets the value of the Value to x.
@@ -30,39 +38,46 @@ func (e *Entry) Store(val any) {
 // Swap stores new into Value and returns the previous value.
 // It returns nil if the Value is empty.
 func (e *Entry) Swap(new any) (old any) {
-	p := atomic.SwapPointer(&e.p, unsafe.Pointer(&new))
-	if p == nil {
-		return nil
-	}
-	return *(*any)(p)
+	// p := atomic.SwapPointer(&e.p, unsafe.Pointer(&new))
+	// if p == nil {
+	// 	return nil
+	// }
+	// return *(*any)(p)
+	return ptr2any(atomic.SwapPointer(&e.p, unsafe.Pointer(&new)))
 }
 
 // CompareAndSwap executes the compare-and-swap operation for the Value.
 func (e *Entry) CompareAndSwap(old, new any) (swapped bool) {
 	for {
 		p := atomic.LoadPointer(&e.p)
-		if p == nil {
-			if old != nil {
-				return false
-			}
-			// old == p == nil
-			if atomic.CompareAndSwapPointer(&e.p, p, unsafe.Pointer(&new)) {
-				return true
-			}
-		} else {
-			// p != nil
-			// runtime_procPin()
-			if *(*any)(p) != old {
-				// runtime_procUnpin()
-				return false
-			}
-			// p == old
-			if atomic.CompareAndSwapPointer(&e.p, p, unsafe.Pointer(&new)) {
-				// runtime_procUnpin()
-				return true
-			}
-			// runtime_procUnpin()
+		if ptr2any(p) != old {
+			return false
 		}
+		if atomic.CompareAndSwapPointer(&e.p, p, unsafe.Pointer(&new)) {
+			return true
+		}
+		// if p == nil {
+		// 	if old != nil {
+		// 		return false
+		// 	}
+		// 	// old == p == nil
+		// 	if atomic.CompareAndSwapPointer(&e.p, p, unsafe.Pointer(&new)) {
+		// 		return true
+		// 	}
+		// } else {
+		// 	// p != nil
+		// 	// runtime_procPin()
+		// 	if *(*any)(p) != old {
+		// 		// runtime_procUnpin()
+		// 		return false
+		// 	}
+		// 	// p == old
+		// 	if atomic.CompareAndSwapPointer(&e.p, p, unsafe.Pointer(&new)) {
+		// 		// runtime_procUnpin()
+		// 		return true
+		// 	}
+		// 	// runtime_procUnpin()
+		// }
 	}
 }
 
